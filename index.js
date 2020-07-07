f8ab86344cd2453b1e62f9fa9badf6f47cfc48ec
ShaoNianyr <licetianyr@163.com>
2020-07-07 18:21:22 +0800

    移除 index.js 弃用代码

--- index.js
+++ index.js
@@ -12,54 +12,6 @@ const scriptPath = './puppeteer/';
         headless: true
     });
     
-    // console.log('-------- 正常监听 request 和 reponse --------');
-    // try {
-    //     listen_url = 'http://localhost:5000/';
-    //     var page = await browser.newPage();
-    //     await page.setRequestInterception(true);
-    //     scripts_listen(page, listen_url);
-    //     await page.goto('http://localhost:5000/');
-    //     await page.waitFor(1000);
-    //     await page.close();
-    // } catch (error) {
-    //     await page.close();
-    // }
-
-    // console.log('--------- 模拟 request timeout 处理 ---------');
-    // try {
-    //     timeout_url = 'http://localhost:5000/';
-    //     var page = await browser.newPage();
-    //     await page.setRequestInterception(true);
-    //     scripts_abort(page, timeout_url, "timedout");
-    //     await page.goto('http://localhost:5000/');
-    // } catch (error) {
-    //     await page.close();
-    // }
-
-    // console.log('--------- 模拟 request aborted 处理 ---------');
-    // try {
-    //     aborted_url = 'http://localhost:5000/';
-    //     var page = await browser.newPage();
-    //     await page.setRequestInterception(true);
-    //     scripts_abort(page, aborted_url, "aborted");
-    //     await page.goto('http://localhost:5000/');
-    // } catch (error) {
-    //     await page.close();
-    // }
-
-    // console.log('------------- mock reponse 处理 -------------');
-    // try {
-    //     mock_url = 'http://localhost:5000/';
-    //     var page = await browser.newPage();
-    //     await page.setRequestInterception(true);
-    //     scripts_mock(page, mock_url, 500, 'text/plain', 'Mock body what you want!');
-    //     await page.goto('http://localhost:5000/');
-    //     await page.waitFor(1000);
-    //     await page.close();
-    // } catch (error) {
-    //     await page.close();
-    // }
-
     var filesList = [];
     readFileList.getDir(scriptPath, filesList);
 
